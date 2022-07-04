9be8af6514b29749da34ff51f6dd0b358ffdcc6f
Michele <michelemancini2@tiscali.it>
2022-07-04 15:17:59 +0200

    sensori Temperature > 25.5

--- SCS/APP/main.py
+++ SCS/APP/main.py
@@ -508,7 +508,7 @@ async def deviceReceiver_from_SCSbus(jqueqe):
 						temp = rawtemp / 10 
 						device.Set_Stato(temp)
 
-						await scsmqtt.post_to_MQTT("/scsshield/device/" + ndevice + "/status", temp )
+						await scsmqtt.post_to_MQTT("/scsshield/device/" + ndevice + "/status", format(temp, '.1f') )
 
 					# sensori Temperature > 25.5
 					elif((len(trama) == 7) and (trama[1] == b'\xB5') and (type.name == SCS.TYPE_INTERfACCIA.sensori_temperatura.name)):
@@ -516,7 +516,7 @@ async def deviceReceiver_from_SCSbus(jqueqe):
 						temp = rawtemp / 10 + 25.6
 						device.Set_Stato(temp)
 
-						await scsmqtt.post_to_MQTT("/scsshield/device/" + ndevice + "/status", temp )
+						await scsmqtt.post_to_MQTT("/scsshield/device/" + ndevice + "/status", format(temp, '.1f') )
 
 					# campanello
 					elif((len(trama) == 7) and (trama[1] == b'\x91') and (trama[3] == b'\x60') and (trama[4] == b'\x08') and (type.name == SCS.TYPE_INTERfACCIA.campanello_porta.name)):
